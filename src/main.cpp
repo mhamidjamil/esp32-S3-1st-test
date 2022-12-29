dc4208abe0d60e8f445f5beb5b6af9eb4e0c3bee
M Hamid Jamil <m.hamid.jamil99@gmail.com>
2022-12-30 01:49:40 +0500

    unknown_behaviour

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
-}
+#define led LED_BUILTIN
+void setup() { pinMode(led, OUTPUT); }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH);
+  delay(3000);
+  digitalWrite(led, LOW);
+  delay(1000);
 }
